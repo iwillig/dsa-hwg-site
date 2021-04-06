2d00c87592f6902185fc56600899663e88be4a8a
kevinyzchen <54418432+kevinyzchen@users.noreply.github.com>
2021-04-06 16:04:46 -0400

    replace globals with tailwind

--- components/splashScreen.js
+++ components/splashScreen.js
@@ -1,12 +1,31 @@
 import styles from "../styles/Home.module.css";
 import React from "react";
+import Image from 'next/image'
 
 const SplashScreen = (props) => {
   return (
     <>
-      <h1 className={styles.title}>
-        NYC DSA Healthcare Working Group
-      </h1>
+    <div className= " mt-16 mb-24 laptop:mt-32 laptop:mb-40 desktop:mt-36 desktop:mb-48 "> 
+        <div  className="mx-8 h-96 w-480 relative hidden tablet:flex">
+          <Image
+            src="/logo_1440.png"
+            alt="DSA Logo"
+            width={1832}
+            height={288}
+            className = "object-contain"
+            ></Image>
+        </div>
+
+        <div  className="mx-4 h-111 w-184 relative flex tablet:hidden">
+          <Image
+            src="/logo_375.png"
+            alt="DSA Logo"
+            width={343}
+            height={236}
+            className = "object-fill"
+            ></Image>
+        </div>
+    </div>
     </>
   );
 }
