8a4d7386619ffce42f0cda4f21a7f9af693fdc9e
kevinyzchen <54418432+kevinyzchen@users.noreply.github.com>
2021-04-06 20:58:02 -0400

    event border

--- components/upcomingEvents.js
+++ components/upcomingEvents.js
@@ -3,7 +3,7 @@ import EventsList from "./eventList";
 
 const UpComingEvents = () => {
   return (
-    <div className ="border-top border-black border-solid border-2">
+    <div className ="border-0 border-t  border-black border-solid ">
       <h2 className = "ml-8 mt-4 text-header-mobile tablet:text-header font-bold">
         Upcoming Events
       </h2>
