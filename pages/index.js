71877c971c232d0427fe80905fce60e29420cad1
Ivan Willig <iwillig@gmail.com>
2021-04-12 21:35:03 -0400

    Remove reccent press until we complete that compontent

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,6 @@ export default function Home() {
         <NavMenu />
         <SplashScreen />
         <UpComingEvents />
-        <RecentPress/>
         <Signup />
         <DonateLink />
       </main>
