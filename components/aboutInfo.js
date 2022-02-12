f9466b8b4d30e51595ab54c91f21724378c3b2e7
Sam Beller <sammbeller@gmail.com>
2022-02-12 14:06:40 -0500

    First pass on new content

--- components/aboutInfo.js
+++ components/aboutInfo.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const AboutInfo  = (props) => {
-  return (<div><p>{props.description}</p></div>);
+  return (
+    <div>
+      <p>
+        The Healthcare Working Group is a working group of the NYC-DSA Chapter of the Democratic Socialists of America. Our purpose is to fight for health justice and the rights of all people to healthcare. We aim to fight for a healthcare system in which the needs of all people are met and no one can profit from the health or illness of another human being. We seek to build a mass movement to demand a fair, equitable, and universal single-payer healthcare system from our state and federal government, and to act in solidarity with individuals and communities who are fighting against health injustice in all forms. We recognize the detrimental health effects of racism, police violence, housing injustice, war and imperialism, climate change, and government austerity, and we seek to work in concert with other DSA working groups and movements to build a society that protects the health and dignity of all people.
+      </p>
+      <p>
+        This year, our campaign to <a href="https://docs.google.com/document/d/1LI8yFmB_BWXDsrV_YXLu-iuYq_UMsXeM3C8m6vzhF5w/edit">Win Long Term Care and Build Power for the New York Health Act</a> is one of NYC-DSA’s five priority campaigns. Learn more about this campaign <a href="https://docs.google.com/document/d/1LI8yFmB_BWXDsrV_YXLu-iuYq_UMsXeM3C8m6vzhF5w/edit">here</a>. To learn more about our working group’s structure and how we function, check out our <a href="https://docs.google.com/document/d/1Ob4mIwyKz38GhqYoGIW6PsxqsxDMHaQhOJiucvpptdw/edit">internal organization document</a>. You can reach us by email at <a href="mailto:healthcare@socialists.nyc">healthcare@socialists.nyc</a> or <a href="https://join.slack.com/t/nyc-dsa-healthcare/shared_invite/zt-qyf20db9-HFU2a8CJWmS8YgcIW5tQpg">join our slack</a> to stay up-to-date on what we’re up to.
+      </p>
+    </div>
+  );
 };
 
 export default AboutInfo;
